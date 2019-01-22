bf5a80bdc3df063e0d52b93e7c3b29edb89bfff2
pgujva <pgujva@usetech.ru>
2019-01-22 19:11:04 +0300

    eeee

--- usetech_velobike_tests/src/test/java/MenuTests.java
+++ usetech_velobike_tests/src/test/java/MenuTests.java
@@ -1,7 +1,7 @@
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/",plugin = {"pretty","html:build/cucumber-report"})
+@CucumberOptions(features = "src/test/resources/",plugin = {"pretty","html:build/cucumber-report","json:target/cucumber.json"})
 public class MenuTests extends AbstractTestNGCucumberTests {
 
 }
